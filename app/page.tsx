1cf9861114b62b8566e6e82cf53d1d469c9306ed
Greatteacherjubro <wisdomjh100@gmail.com>
2025-11-12 20:31:50 +0900

    feat: main-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
 
+import styles from "./styles/main.module.css";
+
 export default function Home() {
   return (
     <>
-      <div>안녕하세요</div>
+      <section className={styles.section}>
+        <p>[Reactman704 Introduction]</p>
+        <p>(This is a website)</p>
+      </section>
+      <section>
+        <h2>blog</h2>
+
+        <ul></ul>
+      </section>
     </>
   );
 }
