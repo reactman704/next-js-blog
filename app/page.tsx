cd911be1454c2f3fbf7e18c723486a74520cadd8
Greatteacherjubro <wisdomjh100@gmail.com>
2025-11-13 19:12:41 +0900

    feat : mainpage-css수정-251113

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,25 @@
 import Image from "next/image";
 
 import styles from "./styles/main.module.css";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-      <section className={styles.section}>
-        <p>[Reactman704 Introduction]</p>
-        <p>(This is a website)</p>
-      </section>
-      <section>
-        <h2>blog</h2>
+      <div>
+        <Head>
+          <title>Create</title>
+        </Head>
 
-        <ul></ul>
-      </section>
+        <section className={styles.headingMd}>
+          <p>[Reactman704 Introduction]</p>
+          <p>(This is a website)</p>
+        </section>
+        <section className={`${styles.headingMd} ${styles.padding1px}`}>
+          <h2>blog</h2>
+          <ul></ul>
+        </section>
+      </div>
     </>
   );
 }
